682d95397d8c521016cfe58a4debd9c22daea4cf
Blackguz <36176398@uaz.edu.mx>
2022-05-01 18:56:45 -0500

    Se agregaron las apps

--- Axoshop/settings.py
+++ Axoshop/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'Axoshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'axoshop.sqlite3',
     }
 }
 
