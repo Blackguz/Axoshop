2224ed323f115db4f0c5e732c7017cea5de76e9a
Elias Beltran <eliasemilianobg@gmail.com>
2022-05-01 20:59:39 -0500

    Urls de core funcionando

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('entrada', views.entrada, name='entrada'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('comunidad', views.comunidad, name='comunidad'),
+    path('tienda', views.tienda, name='tienda'),
+    path('producto', views.producto, name='producto'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
+
+def comunidad(request):
+    return render(request, 'comunidad.html')
+
+def tienda(request):
+    return render(request, 'tienda.html')
+
+def producto(request):
+    return render(request, 'producto.html')
+
+def entrada(request):
+    return render(request, 'entrada.html')
